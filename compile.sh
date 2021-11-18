2c3803e6f0cbe9a18a66ea0950c75e1d9cf1386a
trankha1655 <phamtruong7a1@gmail.com>
2021-11-18 13:15:35 +0700

    python3

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 cd ./models/post_processing/pa/
-python setup.py build_ext --inplace
+python3 setup.py build_ext --inplace
 cd ../pse/
-python setup.py build_ext --inplace
+python3 setup.py build_ext --inplace
 cd ../../../
